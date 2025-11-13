2e0806f957f9725f8269ed813d09a110e55baaae
thinkpad <bibinlopez36@gmail.com>
2025-11-13 21:25:46 +0530

    Sidebar toggle

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,15 @@
-import { getElement } from './utils.js';
+import { getElement } from "./utils.js"
+
+const toggleNav = getElement(".toggle-nav")
+const sidebarOverlay = getElement(".sidebar-overlay")
+const closeBtn = getElement(".sidebar-close")
+
+toggleNav.addEventListener("click", () => {
+  console.log("hi")
+
+  sidebarOverlay.classList.add("show")
+})
+
+closeBtn.addEventListener("click", () => {
+  sidebarOverlay.classList.remove("show")
+})
